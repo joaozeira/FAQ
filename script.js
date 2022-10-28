29caf75dfbf75abc2b11327c76e2c36cfd9c896e
João Carlos <56844688+joaozeira@users.noreply.github.com>
2022-10-28 17:23:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
 /* função voltar para tela inicial */
-function back(){
-    var back = document.getElementById('questions');
-    var clean = document.getElementById('answers');
-    var c1 = document.getElementById('c1');
-    var c2 = document.getElementById('c2');
-    var c3 = document.getElementById('c3');
-    var c4 = document.getElementById('c4');
-    back.style.display = 'block';
-    clean.style.display = 'none';
-    c1.style.display = 'none';
-    c2.style.display = 'none';
-    c3.style.display = 'none';
-    c4.style.display = 'none';
-
+function back() {
+  var back = document.getElementById("questions");
+  var clean = document.getElementById("answers");
+  var c1 = document.getElementById("c1");
+  var c2 = document.getElementById("c2");
+  var c3 = document.getElementById("c3");
+  var c4 = document.getElementById("c4");
+  back.style.display = "block";
+  clean.style.display = "none";
+  c1.style.display = "none";
+  c2.style.display = "none";
+  c3.style.display = "none";
+  c4.style.display = "none";
 }
 
 /* duvidas */
 
-function duvida1(){
-    var back = document.getElementById('questions');
-    var clean = document.getElementById('answers');
-    var c1 = document.getElementById('c1');
-    back.style.display = 'none';
-    clean.style.display = 'block';    
-    c1.style.display = 'block';
+function duvida1() {
+  var back = document.getElementById("questions");
+  var clean = document.getElementById("answers");
+  var c1 = document.getElementById("c1");
+  back.style.display = "none";
+  clean.style.display = "block";
+  c1.style.display = "block";
 }
 
-function duvida2(){
-    var back = document.getElementById('questions');
-    var clean = document.getElementById('answers');
-    var c2 = document.getElementById('c2');
-    back.style.display = 'none';
-    clean.style.display = 'block';    
-    c2.style.display = 'block';
+function duvida2() {
+  var back = document.getElementById("questions");
+  var clean = document.getElementById("answers");
+  var c2 = document.getElementById("c2");
+  back.style.display = "none";
+  clean.style.display = "block";
+  c2.style.display = "block";
 }
 
-function duvida3(){
-    var back = document.getElementById('questions');
-    var clean = document.getElementById('answers');
-    var c3 = document.getElementById('c3');
-    back.style.display = 'none';
-    clean.style.display = 'block';    
-    c3.style.display = 'block';
+function duvida3() {
+  var back = document.getElementById("questions");
+  var clean = document.getElementById("answers");
+  var c3 = document.getElementById("c3");
+  back.style.display = "none";
+  clean.style.display = "block";
+  c3.style.display = "block";
 }
 
-function duvida4(){
-    var back = document.getElementById('questions');
-    var clean = document.getElementById('answers');
-    var c4 = document.getElementById('c4');
-    back.style.display = 'none';
-    clean.style.display = 'block';    
-    c4.style.display = 'block';
+function duvida4() {
+  var back = document.getElementById("questions");
+  var clean = document.getElementById("answers");
+  var c4 = document.getElementById("c4");
+  back.style.display = "none";
+  clean.style.display = "block";
+  c4.style.display = "block";
 }
 
 /* perfil */
@@ -61,28 +60,28 @@ function duvida4(){
     show.style.display = 'none';    
     profile.style.display = 'block';
 } */
-    
+
 /* link open chat*/
 
-function open_chat(){
-    var link = document.getElementById('open-link');
-    var back = document.getElementById('questions');
-    back.style.display = 'block';
-    link.style.display = 'none';
+function open_chat() {
+  var link = document.getElementById("open-link");
+  var back = document.getElementById("questions");
+  back.style.display = "block";
+  link.style.display = "none";
 }
 
-function close_chat(){
-    var link = document.getElementById('open-link');
-    var back = document.getElementById('questions');
-    var notification = document.getElementById('showNotification');    
-    back.style.display = 'none';
-    link.style.display = 'block';
-    notification.style.display = 'none';
+function close_chat() {
+  var link = document.getElementById("open-link");
+  var back = document.getElementById("questions");
+  var notification = document.getElementById("showNotification");
+  back.style.display = "none";
+  link.style.display = "block";
+  notification.style.display = "none";
 }
 
-function showNotification(){
-   var showNotification = document.getElementById('showNotification');
-   showNotification.style.opacity = '1';
+function showNotification() {
+  var showNotification = document.getElementById("showNotification");
+  showNotification.style.opacity = "1";
 }
 
 function lastCards(){    
@@ -90,12 +89,13 @@ function lastCards(){
     var contact = document.getElementById('contact');
     question.style.display = 'none';
     contact.style.display = 'block';
-}
+} 
 
-function closeLastcard(){
-    var question = document.getElementById('questions');
-    var contact = document.getElementById('contact');    
-    question.style.display = 'block';
-    contact.style.display = 'none';
-    
+
+
+function closeLastcard() {
+  var question = document.getElementById("questions");
+  var contact = document.getElementById("contact");
+  question.style.display = "block";
+  contact.style.display = "none";
 }
